a9148ad22151e79664fea80b1175c8e9920303c1
Apichat Piluk <apichat.career@gmail.com>
2024-09-27 10:22:30 +0700

    set

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  // name of your GitHub repo
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  trailingSlash: true,
+};
 
 export default nextConfig;
